240ea0c7f33c607366f750ca22f4c0afdbb9c7e2
Santiago <alexander.cardenas10@outlook.com>
2022-09-12 18:43:37 -0500

    Create migrations and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.destroy_all
+Greeting.create(message: 'Hello World!1')
+Greeting.create(message: 'Hello World!2')
+Greeting.create(message: 'Hello World!3')
+Greeting.create(message: 'Hello World!4')
+Greeting.create(message: 'Hello World!5')
